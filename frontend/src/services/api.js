8347589ee9844a304bf5884fd422b624b0567900
trentjohnson <zookmurnig@gmail.com>
2025-01-20 14:58:35 -0500

    ure api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
-const API_PREFIX = '/api/v1';
+const API_URL= process.env.API_URL;
+const API_PREFIX = '';
 
 const api = axios.create({
   baseURL: API_URL,
