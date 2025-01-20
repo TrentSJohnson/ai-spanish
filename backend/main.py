4366e0653d7c1a8773d69b57d7896d3d4706be45
trentjohnson <zookmurnig@gmail.com>
2025-01-20 15:06:24 -0500

    allow all

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default development port
+    allow_origins=["*"],  # Vite's default development port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
